8f9b8a01d6e93c9da86b0f0fdb9dbc01812bd510
N-Endy <okafornel@gmail.com>
2021-03-18 09:35:03 +0100

    update of playerPlay function to score scores while playing

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function game() {
             playerChoice = prompt('What is your Choice?', '').toLowerCase();
             // Convert player's choice to capitalization
             playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-        } while (!playerChoice || playerChoice === '') 
+        } while (!playerChoice || playerChoice === '');
         
         if (playerChoice === 'Rock' || playerChoice === 'Paper' || playerChoice === "Scissors") {
             return playerChoice;
@@ -38,39 +38,48 @@ function game() {
         // If computer is rock and player is paper, player wins
         if (computerSelection === 'Rock' && playerSelection === 'Paper') {
             playerScore++;
-            alert("You win! Paper covers Rock.");
+            alert(`You win! Paper covers Rock. 
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Rock' && playerSelection === 'Scissors') {
             // If computer is rock and player is scissors, computer wins
             computerScore++;
-            alert('You lose! Rock breaks Scissors');
+            alert(`You lose! Rock breaks Scissors
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Rock' && playerSelection === 'Rock') {
             // If computer is rock and player is rock, it's a draw
             draw++;
-            alert("It's a draw! You both chose Rock");
+            alert(`It's a draw! You both chose Rock
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Paper' && playerSelection === 'Rock') {
             // If computer is paper and player is rock, computer wins
             computerScore++;
-            alert('You lose! Paper covers Rock');
+            alert(`You lose! Paper covers Rock
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Paper' && playerSelection === 'Scissors') {
             // If computer is paper and player is scissors, player wins
             playerScore++;
-            alert('You win! Scissors cuts Paper');
+            alert(`You win! Scissors cuts Paper
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Paper' && playerSelection === 'Paper') {
             // If computer is paper and player is paper, it's a draw
             draw++;
-            alert("It's a draw. You both chose Paper");
+            alert(`It's a draw. You both chose Paper
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Scissors' && playerSelection === 'Rock') {
             // If computer is scissors and player is rock, player wins
             playerScore++;
-            alert('You win! Rock breaks Scissors');
+            alert(`You win! Rock breaks Scissors
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Scissors' && playerSelection === 'Paper') {
             // If computer is scissors and player is paper, computer wins
             computerScore++;
-            alert('You lose! Scissors cuts Paper');
+            alert(`You lose! Scissors cuts Paper
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         } else if (computerSelection === 'Scissors' && playerSelection === 'Scissors') {
             // If computer is scissors and player is scissors, it's a draw
             draw++;
-            alert("It's a draw! You both chose Scissors")
+            alert(`It's a draw! You both chose Scissors
+                    Player: ${playerScore}  Draw: ${draw}  Computer: ${computerScore}`);
         }
     }
     playRound();
@@ -85,7 +94,7 @@ function game() {
     if (playerScore > computerScore) {
         alert(`You won ${playerScore}, Computer won ${computerScore}, and you drew ${draw} out of 5. You win the game!`);
     } else if (playerScore === computerScore) {
-        alert(`You won ${playerScore}, Computer won ${computerScore}, and you drew ${draw} out of 5. It was a draw`);
+        alert(`You won ${playerScore}, Computer won ${computerScore}, and you drew ${draw} out of 5. It's a draw`);
     } else {
         alert(`You won ${playerScore}, Computer won ${computerScore}, and you drew ${draw} out of 5. You lose the game!`);
     }
